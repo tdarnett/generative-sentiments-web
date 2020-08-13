d11c9480adf9139ea9cd7aa0dd03fa1927873962
Taylor Arnett <tarnett1@mac.com>
2020-08-12 23:41:26 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <SentimentInput msg="Describe how you feel in one sentence..." />
+  </div>
+</template>
+
+<script>
+import SentimentInput from "./components/SentimentInput.vue";
+
+export default {
+  name: "App",
+  components: {
+    SentimentInput,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
